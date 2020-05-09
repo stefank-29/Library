ff67d51470e4cbe9ddb2ae4c4b7b13cf4af63a8e
stefan <stefankaraferovic@gmail.com>
2020-05-10 01:22:52 +0200

    Empty library and rendering dom elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+const booksGrid = document.querySelector("#books");
+
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -10,10 +12,55 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`;
 }
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const theHobbit2 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const theHobbit3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const anaKarenjina = new Book("Ana Karenjina", "Lav Tolstoj", 745, true);
+
 //console.log(theHobbit.info());
 addBookToLibrary(theHobbit);
+addBookToLibrary(theHobbit2);
+addBookToLibrary(theHobbit3);
+addBookToLibrary(anaKarenjina);
+
 //console.table(myLibrary);
 
 function addBookToLibrary(book){
     myLibrary.push(book);
+}
+
+function removeBooks(){ 
+    const booksGrid = document.querySelector("#books");
+    let child = booksGrid.lastElementChild;
+    while(child){
+        booksGrid.removeChild(child);
+        child = booksGrid.lastElementChild;
+    }
+
+}
+function render(){
+    removeBooks();
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add("book");
+        const title = document.createElement('p');
+        title.classList.add("title");
+        title.textContent = book.title;
+        const author = document.createElement('p');
+        author.classList.add("author");
+        author.textContent = `Author: ${book.author}`;
+        const pages = document.createElement('p');
+        pages.classList.add("pages");
+        pages.textContent = `Pages: ${book.pages}`;
+        const read = document.createElement('p');
+        read.classList.add("read");
+        read.textContent = `Read: ${read ? "Yes" : "Not already read"}`;
+        
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        booksGrid.appendChild(card);
+        
+    });
+
 }
